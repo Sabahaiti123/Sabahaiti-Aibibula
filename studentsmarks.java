6ac420f1376994c5958ef46967677effda943e49
Sabahaiti123 <a.sabahaiti.10@student.scu.edu.au>
2024-11-08 20:29:46 +1000

    Initial commit

---
+++ studentsmarks.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class studentsmarks here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class studentsmarks
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class studentsmarks
+     */
+    public studentsmarks()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
