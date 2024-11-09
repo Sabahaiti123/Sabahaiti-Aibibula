f1388a3a11f85f99b2b9c656033bd7c1f0308938
Sabahaiti123 <a.sabahaiti.10@student.scu.edu.au>
2024-11-09 13:16:20 +1000

    finish Algorithm1
    highest and lowest marks

--- Algorithm1.java
+++ Algorithm1.java
@@ -9,9 +9,46 @@ import java.util.Scanner;
 public class Algorithm1
 {
     public static void main(String[] args){
-      
-    Scanner scanner = new Scanner(System.in);
-    int[] marks = new int[15]; 
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int numberOfStudents = 30;
+        
+        double[] marks = new double[numberOfStudents];
+        
+        double lowestmarks = marks[0];
+        double highestmarks = marks[0];
+        
+        System.out.println("Give marks for 30 students");
+        
+        for(int i=0; i<numberOfStudents; i++){
+            double temp = scanner.nextDouble();
+            
+            while(temp < 0 || temp > 30){
+                
+                System.out.println("Invalid marks, Please enter a value between 0 and 30.");
+                continue;
+                
+            }
+            
+            marks[i] = temp;
+            if(lowestmarks > temp){
+                
+                lowestmarks = temp;
+                
+            }
+            if(highestmarks < temp){
+                
+                highestmarks = temp;
+                        
+            }
+    }
+    
+    System.out.println("lowestmarks");
+    System.out.println(lowestmarks);
+    System.out.println("highestmarks");
+    System.out.println(highestmarks);
     
 }
 }
+
